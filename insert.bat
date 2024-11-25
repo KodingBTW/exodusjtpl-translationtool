9890799426360939fd639ca900e6554fdcd2b5ba
Rodrigo Muñoz <83840135+KodingBTW@users.noreply.github.com>
2024-11-24 22:20:09 -0300

    Update insert.bat

--- insert.bat
+++ insert.bat
@@ -5,7 +5,7 @@ set tblFile="encoder.tbl"
 set pointersStartAddress=0x144F4
 set textStartAddress=0x1007B
 set headerSize=0x8010
-set textSize=4479
+set textSize=0x4479
 :loop
 	pause
 	HexString -e %textFile% %textStartAddress% %textSize% %pointersStartAddress% %headerSize% %romName% %tblFile%
